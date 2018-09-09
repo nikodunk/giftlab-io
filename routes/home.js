7ec454cb4cd12e2048e2d571fb52d6782742ffc2
Nikolai <n.dunkel@gmail.com>
2018-09-08 18:09:09 -0700

    pg heroku updated

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,9 @@ var router = express.Router();
 var content = require('../public/javascripts/content.json')
 var giftlists = require('../public/javascripts/giftlists.json')
 var links = require('../public/javascripts/links.json')
-var pg = require('pg');
+const { Pool } = require('pg')
+const pool = new Pool()
+
 
 
 /* GET home page. */
@@ -27,7 +29,7 @@ router.get('/contact', function(req, res, next) {
 
 
 router.post( '/signup', function (req, res) {
-	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+	pool.connect(process.env.DATABASE_URL, function(err, client, done) {
 		client.query(`INSERT INTO marketing VALUES ('`+req.body.email+`');`)
 		done();
 		res.send( 'added' )
