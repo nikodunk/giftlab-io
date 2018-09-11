16a1ec09ea3b5828bfac004ac8731ce6ab6ab46c
Nikolai <n.dunkel@gmail.com>
2018-09-10 23:52:18 -0700

    newest source deploy Mon Sep 10 23:52:18 PDT 2018

--- routes/paypal.js
+++ routes/paypal.js
@@ -14,36 +14,33 @@ paypal.configure({
 router.post('/submit', (req, res) => {
   let description = req.body.description ? req.body.description : 'This is the payment description'
   let amount = req.body.amount
-  let host = req.protocol + '://' + req.get('host')
-  let createPaymentJson = {
-    intent: "sale", // authorize
-    payer: {
-      payment_method: "paypal"
-    },
-    redirect_urls: {
-      return_url: host + '/paypal/payment-return',
-      cancel_url: host + '/paypal/payment-cancel'
-    },
-    transactions: [{
-      item_list: {
-        items: [{
-          name: "Donation",
-          sku: "d1",
-          price: amount,
-          currency: "USD",
-          quantity: 1
-        }]
+  // let host = req.protocol + '://' + req.get('host')
+  var create_payment_json = {
+      "intent": "sale",
+      "payer": {
+          "payment_method": "paypal"
       },
-      amount: {
-        currency: "USD",
-        total: amount
+      "redirect_urls": {
+          "return_url": "http://return.com",
+          "cancel_url": "http://cancel.com"
       },
-      payee: {
-            email: 'info@seaturtles.org'
-        },
-      description: 'Your donation to this location'
-    }]
-  }
+      "transactions": [{
+          "item_list": {
+              "items": [{
+                  "name": "item",
+                  "sku": "item",
+                  "price": "1.00",
+                  "currency": "USD",
+                  "quantity": 1
+              }]
+          },
+          "amount": {
+              "currency": "USD",
+              "total": "1.00"
+          },
+          "description": "This is the payment description."
+      }]
+  };
 
   // SAVE TO DATABASE
     // create orderID = 'timestamp'
@@ -53,27 +50,15 @@ router.post('/submit', (req, res) => {
     // });
 
   // Call PayPal to process the payment
-  paypal.payment.create(createPaymentJson, (err, payment) => {
-    if (err) {
-      console.log(err.response.error_description)
-      throw err
-    } else {
-      console.log("Create Payment response...")
-      console.log(payment)
-      let redirectUrl
-      payment.links.forEach((link) => {
-        if (link.method === 'REDIRECT') {
-          redirectUrl = link.href
-        }
-      })
-      if (redirectUrl) {
-        res.status(200).redirect(redirectUrl)
+  paypal.payment.create(create_payment_json, function (error, payment) {
+      if (error) {
+          throw error;
       } else {
-        logger.error('Cannot find redirect url from paypal payment result!')
+          console.log("Create Payment Response");
+          console.log(payment);
       }
-    }
-   })
-  
+  });
+
 })
 
 router.get('/payment-return', function(req, res, next) {
