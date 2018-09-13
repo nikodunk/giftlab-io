3b21dd8ef2b1142b9412d6a2c0f4dcef2a34c259
Nikolai <n.dunkel@gmail.com>
2018-09-13 02:16:53 -0700

    newest source deploy Thu Sep 13 02:16:53 PDT 2018

--- routes/projects.js
+++ routes/projects.js
@@ -39,7 +39,7 @@ function getData(projectNumber, res){
                             }
                                 
                       res.render('project', 
-                          { content: content[projectNumber],
+                          { content: content[projectNumber-1],
                             skus: skuList
                           }
                       );
@@ -53,56 +53,12 @@ router.get('/leatherbacks/', function(req, res, next) {
 });
 
 router.get('/salmon/', function(req, res, next) {
-    getData(2, (result) => {res.render('project', 
-                                  { content: content[1],
-                                    skus: result
-                                  }
-                            );
-                        } 
-    )
+    getData(2, res)
 });
 
 
-
 router.get('/microplastics/', function(req, res, next) {
-  client.query(`
-                    SELECT skus.sku, skus.sku_name, skus.bucket, skus.description, skus.status, skus.timeline, skus.priceperunitusd, skus.quantityneeded, skus.totalcostusd, sum(orders.amount) FROM skus 
-                    FULL OUTER JOIN orders ON (skus.sku = orders.sku) 
-                    WHERE projectid = '3'
-                    GROUP BY skus.sku;`, (err, queryResult) => {
-
-                    var skuList = []
-
-                    for (let row of queryResult.rows) {
-                              // Create an object to save current row's data
-                              var sku = {
-                                'sku_name': row.sku_name,
-                                'bucket': row.bucket,
-                                "sku": row.sku,
-                                "description": row.description,
-                                "status": row.status,
-                                "timeline": row.timeline,
-                                "priceperunitusd": row.priceperunitusd,
-                                "quantityneeded": row.quantityneeded,
-                                "totalcostusd": row.totalcostusd,
-                                "donatedsofar": row.sum
-                              }
-                              // Add object into array
-                              skuList.push(sku);
-                          }
-
-                    console.log(skuList)
-
-                    res.render('project', 
-                                { content: content[2],
-                                  skus: skuList
-                                }
-                              );
-
-                    client.end();
-
-                    })
-  
+  getData(3, res)
 });
 
 
