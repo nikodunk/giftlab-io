71b2f7e4baddfb8841f4558cd2c051006b5ae676
Nikolai <n.dunkel@gmail.com>
2018-09-13 01:52:57 -0700

    newest source deploy Thu Sep 13 01:52:57 PDT 2018

--- routes/projects.js
+++ routes/projects.js
@@ -11,7 +11,41 @@ client.connect();
 var content = require('../public/javascripts/content.json')
 
 
+var getData = function(projectNumber){
+    client.query(`
+                      SELECT skus.sku, skus.sku_name, skus.bucket, skus.description, skus.status, skus.timeline, skus.priceperunitusd, skus.quantityneeded, skus.totalcostusd, sum(orders.amount) FROM skus 
+                      FULL OUTER JOIN orders ON (skus.sku = orders.sku) 
+                      WHERE projectid = '`projectNumber`'
+                      GROUP BY skus.sku;`, (err, queryResult) => {
 
+                      var skuList = []
+
+                      for (let row of queryResult.rows) {
+                                // Create an object to save current row's data
+                                var sku = {
+                                  'sku_name': row.sku_name,
+                                  'bucket': row.bucket,
+                                  "sku": row.sku,
+                                  "description": row.description,
+                                  "status": row.status,
+                                  "timeline": row.timeline,
+                                  "priceperunitusd": row.priceperunitusd,
+                                  "quantityneeded": row.quantityneeded,
+                                  "totalcostusd": row.totalcostusd,
+                                  "donatedsofar": row.sum
+                                }
+                                // Add object into array
+                                skuList.push(sku);
+                            }
+
+                      // console.log(skuList)
+
+                      return skuList
+
+                      client.end();
+
+                      })
+}
 
 router.get('/leatherbacks/', function(req, res, next) {
     getData(1, (res) => {res.render('project', 
@@ -77,41 +111,7 @@ router.get('/microplastics/', function(req, res, next) {
 });
 
 
-getData(projectNumber){
-    client.query(`
-                      SELECT skus.sku, skus.sku_name, skus.bucket, skus.description, skus.status, skus.timeline, skus.priceperunitusd, skus.quantityneeded, skus.totalcostusd, sum(orders.amount) FROM skus 
-                      FULL OUTER JOIN orders ON (skus.sku = orders.sku) 
-                      WHERE projectid = '`projectNumber`'
-                      GROUP BY skus.sku;`, (err, queryResult) => {
 
-                      var skuList = []
-
-                      for (let row of queryResult.rows) {
-                                // Create an object to save current row's data
-                                var sku = {
-                                  'sku_name': row.sku_name,
-                                  'bucket': row.bucket,
-                                  "sku": row.sku,
-                                  "description": row.description,
-                                  "status": row.status,
-                                  "timeline": row.timeline,
-                                  "priceperunitusd": row.priceperunitusd,
-                                  "quantityneeded": row.quantityneeded,
-                                  "totalcostusd": row.totalcostusd,
-                                  "donatedsofar": row.sum
-                                }
-                                // Add object into array
-                                skuList.push(sku);
-                            }
-
-                      // console.log(skuList)
-
-                      return skuList
-
-                      client.end();
-
-                      })
-}
 
 
 
