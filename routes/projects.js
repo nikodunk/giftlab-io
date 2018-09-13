3e0b984c2676880bdc1f936c086cee48b041e4dd
Nikolai <n.dunkel@gmail.com>
2018-09-13 01:39:44 -0700

    newest source deploy Thu Sep 13 01:39:44 PDT 2018

--- routes/projects.js
+++ routes/projects.js
@@ -15,19 +15,83 @@ var skus = require('../public/javascripts/skus.json')
 
 
 router.get('/leatherbacks/', function(req, res, next) {
-  res.render('project', 
-              { content: content[0],
-                skus: skus
-              }
-            );
+  client.query(`
+                      SELECT skus.sku, skus.sku_name, skus.bucket, skus.description, skus.status, skus.timeline, skus.priceperunitusd, skus.quantityneeded, skus.totalcostusd, sum(orders.amount) FROM skus 
+                      FULL OUTER JOIN orders ON (skus.sku = orders.sku) 
+                      WHERE projectid = '1'
+                      GROUP BY skus.sku;`, (err, queryResult) => {
+
+                      var skuList = []
+
+                      for (let row of queryResult.rows) {
+                                // Create an object to save current row's data
+                                var sku = {
+                                  'sku_name': row.sku_name,
+                                  'bucket': row.bucket,
+                                  "sku": row.sku,
+                                  "description": row.description,
+                                  "status": row.status,
+                                  "timeline": row.timeline,
+                                  "priceperunitusd": row.priceperunitusd,
+                                  "quantityneeded": row.quantityneeded,
+                                  "totalcostusd": row.totalcostusd,
+                                  "donatedsofar": row.sum
+                                }
+                                // Add object into array
+                                skuList.push(sku);
+                            }
+
+                      console.log(skuList)
+
+                      res.render('project', 
+                                  { content: content[0],
+                                    skus: skuList
+                                  }
+                                );
+
+                      client.end();
+
+                      })
 });
 
 router.get('/salmon/', function(req, res, next) {
-  res.render('project', 
-              { content: content[1],
-  							skus: skus
-              }
-            );
+  client.query(`
+                      SELECT skus.sku, skus.sku_name, skus.bucket, skus.description, skus.status, skus.timeline, skus.priceperunitusd, skus.quantityneeded, skus.totalcostusd, sum(orders.amount) FROM skus 
+                      FULL OUTER JOIN orders ON (skus.sku = orders.sku) 
+                      WHERE projectid = '2'
+                      GROUP BY skus.sku;`, (err, queryResult) => {
+
+                      var skuList = []
+
+                      for (let row of queryResult.rows) {
+                                // Create an object to save current row's data
+                                var sku = {
+                                  'sku_name': row.sku_name,
+                                  'bucket': row.bucket,
+                                  "sku": row.sku,
+                                  "description": row.description,
+                                  "status": row.status,
+                                  "timeline": row.timeline,
+                                  "priceperunitusd": row.priceperunitusd,
+                                  "quantityneeded": row.quantityneeded,
+                                  "totalcostusd": row.totalcostusd,
+                                  "donatedsofar": row.sum
+                                }
+                                // Add object into array
+                                skuList.push(sku);
+                            }
+
+                      console.log(skuList)
+
+                      res.render('project', 
+                                  { content: content[1],
+                                    skus: skuList
+                                  }
+                                );
+
+                      client.end();
+
+                      })
 });
 
 
