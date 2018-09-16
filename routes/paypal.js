7a7ffb3d3a38f08793533621162d1fdf9362b74e
Nikolai <n.dunkel@gmail.com>
2018-09-15 20:38:02 -0700

    newest source deploy Sat Sep 15 20:38:02 PDT 2018

--- routes/paypal.js
+++ routes/paypal.js
@@ -18,7 +18,7 @@ var PAYPAL_ID =  process.env.PAYPAL_ID;
    // 'AUJoKVGO3q1WA1tGgAKRdY6qx0qQNIQ6vl6D3k7y64T4qh5WozIQ7V3dl3iusw5BwXYg_T5FzLCRguP8';
 var PAYPAL_CLIENT_SECRET =  process.env.PAYPAL_CLIENT_SECRET
     // 'EOw8LNwDhM7esrQ3nHfzKc7xiWnJc83Eawln4YLfUgivfx1LGzu9Mj0F5wlarilXDqdK9Q5aHVo-VGjJ';
-var PAYPAL_API = 'https://api.paypal.com';
+var PAYPAL_API = 'https://api.sandbox.paypal.com';
 
 
 
