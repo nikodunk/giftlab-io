0361b4d843cb8b0ee90773d3be50b4b2a6919c23
Nicholas Dunkel <nicholasdunkel@NicholasDunkel.local>
2018-09-17 15:32:28 -0700

    newest source deploy Mon Sep 17 15:32:28 PDT 2018

--- routes/projects.js
+++ routes/projects.js
@@ -10,8 +10,8 @@ client.connect();
 
 var content = require('../public/javascripts/content.json')
 
-var Mixpanel = require('mixpanel');
-var mixpanel = Mixpanel.init('bba2c237fccc04b19cec51dee15cf123');
+// var Mixpanel = require('mixpanel');
+// var mixpanel = Mixpanel.init('bba2c237fccc04b19cec51dee15cf123');
 
 
 
@@ -71,23 +71,23 @@ function getData(projectNumber, res){
 
 router.get('/leatherbacks/', function(req, res, next) {
     getData(1, res)
-    mixpanel.track('leatherbacks_loaded');
+    // mixpanel.track('leatherbacks_loaded');
 });
 
 router.get('/salmon/', function(req, res, next) {
     getData(2, res)
-    mixpanel.track('salmon_loaded');
+    // mixpanel.track('salmon_loaded');
 });
 
 
 router.get('/microplastics/', function(req, res, next) {
   getData(3, res)
-  mixpanel.track('microplastics_loaded');
+  // mixpanel.track('microplastics_loaded');
 });
 
 router.get('/tigersharks/', function(req, res, next) {
   getData(4, res)
-  mixpanel.track('tigersharks_loaded');
+  // mixpanel.track('tigersharks_loaded');
 });
 
 
