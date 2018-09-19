b0c4a2a9f3b92031504c76f93e9b825bdfc97099
Nikolai <n.dunkel@gmail.com>
2018-09-19 12:29:19 -0700

    newest source deploy Wed Sep 19 12:29:19 PDT 2018

--- routes/paypal.js
+++ routes/paypal.js
@@ -13,21 +13,24 @@ var stripe = require("stripe")("sk_test_FufIvJxq2f94m1QAt1T12wMR");
 
 
   router.post('/charge/:sku/:amount/', (req, res) => {
+
     let token = req.body.stripeToken
     console.assert(token)
     const amount = req.params.amount * 100
+    
     stripe.charges.create({
       amount: amount,
       currency: 'usd',
       source: token,
       description: 'Stripe experiment testing charge'
     }, (err, charge) => {
-      if (err) {
-        res.redirect('/stripe/payment-failure?err_msg=' + err.message)
-      } else {
+      if (err) { res.redirect('/stripe/payment-failure?err_msg=' + err.message) } 
+
+      else {
         console.log('Charged successful')
         console.log('charge', charge)
-        res.redirect('/stripe/payment-success/' + charge.id)
+        console.log('SAVE TO DATABASE:' + req.params.sku, charge.amount, charge.receipt_email)
+        // res.redirect('/stripe/payment-success/' + charge.id)
       }
     })
   })
