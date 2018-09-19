aa5afa96941e2a8a18ba94ef9a011f2a4dc77725
Nikolai <n.dunkel@gmail.com>
2018-09-19 10:40:29 -0700

    newest source deploy Wed Sep 19 10:40:29 PDT 2018

--- routes/paypal.js
+++ routes/paypal.js
@@ -12,34 +12,47 @@ client.connect();
 var stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
 
 
-  // Set up the payment:
-  // 1. Set up a URL to handle requests from the PayPal button
-  router.post('/create-payment/:sku/:amount/', function(req, res){
-
-    
-    console.log('WORKING BACKEND')
-
-
+  router.get('/charge/:token', (req, res) => {
+    let token = req.params.token
+    console.assert(token)
+    const amount = CHARGE_AMOUNT * 100
     stripe.charges.create({
-      amount: 1000,
-      currency: "usd",
-      source: "tok_visa",
-      destination: {
-                account: "{CONNECTED_STRIPE_ACCOUNT_ID}",
-              },
-    }).then(function(charge) {
-      // asynchronously called
-
-    });
-
-
+      amount: amount,
+      currency: 'usd',
+      source: token,
+      description: 'Stripe experiment testing charge'
+    }, (err, charge) => {
+      if (err) {
+        res.redirect('/stripe/payment-failure?err_msg=' + err.message)
+      } else {
+        console.log('charge', charge)
+        if (charge.outcome && charge.outcome.risk_level != 'normal') {
+          res.redirect('/stripe/payment-warning?charge_id=' + charge.id + '&msg=' + charge.outcome.seller_message)
+        } else {
+          res.redirect('/stripe/payment-success/' + charge.id)
+        }
+      }
+    })
   })
 
-  // Execute the payment:
-  // 1. Set up a URL to handle requests from the PayPal button.
-  router.post('/execute-payment/:sku/:amount/', function(req, res) {
-
-  
+  router.post('/charge', (req, res) => {
+    let token = req.body.stripeToken
+    console.assert(token)
+    const amount = CHARGE_AMOUNT * 100
+    stripe.charges.create({
+      amount: amount,
+      currency: 'usd',
+      source: token,
+      description: 'Stripe experiment testing charge'
+    }, (err, charge) => {
+      if (err) {
+        res.redirect('/stripe/payment-failure?err_msg=' + err.message)
+      } else {
+        console.log('Charged successful')
+        console.log('charge', charge)
+        res.redirect('/stripe/payment-success/' + charge.id)
+      }
+    })
   })
 
 
