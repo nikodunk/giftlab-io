54a8109059b204eeed724912908c6138d6fb3bff
Nikolai <n.dunkel@gmail.com>
2018-09-20 11:21:45 -0700

    newest source deploy Thu Sep 20 11:21:45 PDT 2018

--- routes/payment.js
+++ routes/payment.js
@@ -27,9 +27,35 @@ var stripe = require("stripe")("sk_test_FufIvJxq2f94m1QAt1T12wMR");
 
       else {
         console.log('Charged successful')
-        console.log('charge', charge)
         console.log('SAVE TO DATABASE:' + req.params.sku, charge.amount, charge.source.name)
-        res.send('successful')
+        
+        // client.query(`INSERT INTO orders VALUES ('`+
+        //                     Date.now()+`','`+
+        //                     response.body.transactions[0].item_list.items[0].sku+`',`+
+        //                     response.body.transactions[0].amount.total+`,'`+
+        //                     response.body.payer.payer_info.email+`','`+
+        //                     response.body.id+`','`+
+        //                     ''+`','`+ //amazon_orderid
+        //                     response.body.payer.payer_info.first_name+`','`+
+        //                     response.body.payer.payer_info.last_name+`','`+
+        //                     response.body.create_time+`','`+
+        //                     'Paypal'+`','`+
+        //                     response.body.payer.payer_info.shipping_address.country_code+`','`+
+        //                     response.body.transactions[0].amount.currency+`','`+
+        //                     ''+`','`+ //thankyou_link
+        //                     ''+`','`+ //donation_receipt
+        //                     response.body.create_time+`','`+
+        //                     response.body.payer.payer_info.payer_id+`','`+
+        //                     response.body.payer.payer_info.shipping_address.postal_code+`','`+
+        //                     response.body.transactions[0].payee.email+`');`)
+
+        // 4. Return a success response to the client
+        // mixpanel.track('paypal_finished');
+        res.json(
+        {
+          status: 'success',
+          response: charge
+        });
         // res.redirect('/payment/payment-success/' + charge.id)
       }
     })
