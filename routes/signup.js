584111109f3bd44690b6a07bfccbee5654ce4641
Nicholas Dunkel <nicholasdunkel@nicholasdunkel.lan>
2018-10-02 20:30:30 +0200

    newest source deploy Tue Oct  2 20:30:30 CEST 2018

--- routes/signup.js
+++ routes/signup.js
@@ -13,27 +13,38 @@ var request = require('request');
 
 
 
-
-
-
-
 router.get('/', function(req, res, next) {
     res.render('signup');
 });
 
 
+
 router.get('/success/', function(req, res, next) {
     console.log('THIS IS THE AUTH CODE: ----> '+ req.query.code)
     
-    request.post(
-        'https://connect.stripe.com/oauth/token',
-        { json: { client_secret: 'sk_test_FufIvJxq2f94m1QAt1T12wMR', code: req.query.code, grant_type: 'authorization_code'  } },
+    request.post( 'https://connect.stripe.com/oauth/token', {
+	    	json: { 
+	        	client_secret: 'sk_test_FufIvJxq2f94m1QAt1T12wMR', 
+	        	code: req.query.code, 
+	        	grant_type: 'authorization_code'  
+	        }
+    	},
         function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 console.log(body)
+                // client.query(`INSERT INTO stripe_codes VALUES ('`+body.stripe_user_id+`');`); 
             }
         }
     );
+
+    // request.post({
+    //   url: 'https://connect.stripe.com/oauth/deauthorize',
+    //   formData: {
+    //     client_id: 'ca_Dcj8reHZ6xjlEBfDVn4Zl07ergfiWVCG',
+    //     stripe_user_id: 'acct_m8CpCJwOdqmHKu',
+    //   },
+    //   headers: {'Authorization': 'Bearer sk_test_FufIvJxq2f94m1QAt1T12wMR'},
+    // });
 
 
     // curl https://connect.stripe.com/oauth/token \
@@ -42,6 +53,8 @@ router.get('/success/', function(req, res, next) {
     //    -d grant_type=authorization_code
 
     // client.query(`INSERT INTO marketing VALUES ('`+req.body.email+`');`); 
+
+
     res.render('signup_success');
 });
 
