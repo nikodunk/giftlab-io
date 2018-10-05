78c4cc9fca8c6dd12170b4016e9e8f28e1d133d2
Nicholas Dunkel <nicholasdunkel@NicholasDunkel.local>
2018-10-05 16:53:15 -0700

    newest source deploy Fri Oct  5 16:53:15 PDT 2018

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo -e "\033[0;32mDeploying updates to GitHub Master Branch...\033[0m"
+echo -e "\033[0;32mDeploying updates to GitHub Develop Branch...\033[0m"
 
 git add .
 git commit -m "newest source deploy `date`"
-git push origin master
+git push origin develop
