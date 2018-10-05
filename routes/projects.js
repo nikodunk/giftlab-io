35060e61886cfd9584d977bed05fb48ffa0bba89
Nikolai <n.dunkel@gmail.com>
2018-10-04 23:21:36 -0700

    newest source deploy Thu Oct  4 23:21:36 PDT 2018

--- routes/projects.js
+++ routes/projects.js
@@ -20,7 +20,83 @@ const numberWithCommas = (x) => {
 }
 
 
-function getData(projectNumber, res){
+
+
+
+
+router.get('/leatherbacks/', function(req, res, next) {
+    getData(1, res)
+    // mixpanel.track('leatherbacks_loaded');
+});
+
+router.get('/salmon/', function(req, res, next) {
+    getData(2, res)
+    // mixpanel.track('salmon_loaded');
+});
+
+
+router.get('/microplastics/', function(req, res, next) {
+  getData(3, res)
+  // mixpanel.track('microplastics_loaded');
+});
+
+
+router.get('/tigersharks/', function(req, res, next) {
+  getData(4, res)
+  // mixpanel.track('tigersharks_loaded');
+});
+
+
+
+function getDataComplete(projectNumber, res){
+    
+    client.query(` SELECT 
+                      skus.sku, 
+                      skus.sku_name, 
+                      skus.bucket, 
+                      skus.description, 
+                      skus.unit_cost,
+                      skus.total_need,
+                      sum(orders.amount) as orderssofar 
+
+                      FROM skus 
+                      FULL OUTER JOIN orders ON (skus.sku = orders.sku) 
+                      WHERE projectid = '`+projectNumber+`'
+                      GROUP BY skus.sku;`, (err, queryResult) => {
+                      if(err){console.log(err)}
+
+                      var skuList = []
+
+                      for (let row of queryResult.rows) {
+                                // Create an object to save current row's data
+                                var sku = {
+                                  "sku": row.sku,
+                                  'sku_name': row.sku_name,
+                                  'bucket': row.bucket,
+                                  "description": row.description,
+                                  "unit_cost": row.unit_cost,
+                                  "donatedsofar": row.orderssofar,
+                                  "donatedsofar_pretty": numberWithCommas(row.orderssofar ? parseInt(row.orderssofar) : 0),
+                                  "total_need": row.total_need,
+                                  "status": (parseInt(row.total_need) - (row.orderssofar ? row.orderssofar : 0) > 0 ? "Active" : "Complete"),
+                                  "remaining": parseInt(row.total_need) - (row.orderssofar ? parseInt(row.orderssofar) : 0),
+                                  "remaining_pretty": numberWithCommas(parseInt(row.total_need) - (row.orderssofar ? parseInt(row.orderssofar) : 0))
+                                }
+                                // Add object into array
+                                skuList.push(sku);
+                            }
+                            // console.log(skuList)
+                      res.render('projectComplete', 
+                          { content: content[projectNumber-1],
+                            skus: skuList
+                          }
+                      );
+                })
+}
+
+
+
+function getDataComplete(projectNumber, res){
     
     client.query(` SELECT 
                       skus.sku, 
@@ -65,31 +141,6 @@ function getData(projectNumber, res){
                       );
                 })
 }
-
-
-
-router.get('/leatherbacks/', function(req, res, next) {
-    getData(1, res)
-    // mixpanel.track('leatherbacks_loaded');
-});
-
-router.get('/salmon/', function(req, res, next) {
-    getData(2, res)
-    // mixpanel.track('salmon_loaded');
-});
-
-
-router.get('/microplastics/', function(req, res, next) {
-  getData(3, res)
-  // mixpanel.track('microplastics_loaded');
-});
-
-router.get('/tigersharks/', function(req, res, next) {
-  getData(4, res)
-  // mixpanel.track('tigersharks_loaded');
-});
-
-
 
 
 module.exports = router;
